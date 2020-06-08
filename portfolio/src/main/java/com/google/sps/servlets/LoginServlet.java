23892a3853b56fb950919d2763e2fc86058a7b48
Andrew Li <andrewhanzhuoli@gmail.com>
2020-06-08 15:44:29 +0000

    Add login servlet for detecting user login status

---
+++ portfolio/src/main/java/com/google/sps/servlets/LoginServlet.java
@@ -0,0 +1,38 @@
+package com.google.sps.servlets;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    UserService userService = UserServiceFactory.getUserService();
+    PrintWriter writer = response.getWriter();
+
+    if (userService.isUserLoggedIn()) {
+      writer.println("The user is logged in!");
+    } else {
+      writer.println("The user is not logged in!");
+      String loginUrl = userService.createLoginURL("/login");
+      writer.println("<p>Login <a href=\"" + loginUrl + "\">here</a>.</p>");
+    }
+  }
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+  }
+}
